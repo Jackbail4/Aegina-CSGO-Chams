3eee0c480a24e826c3fcc44054607b335253183b
Jackbail4 <64487175+Jackbail4@users.noreply.github.com>
2022-01-31 23:02:13 +0000

    Update IVModeInfoClient.h

--- Aegina CSGO Visuals/IVModeInfoClient.h
+++ Aegina CSGO Visuals/IVModeInfoClient.h
@@ -4,7 +4,5 @@
 class IVModelInfoClient
 {
 public:
-    VIRTUAL_METHOD(void*, GetModel, 1, (int Index), (this, Index))
-    VIRTUAL_METHOD(int, GetModelIndex, 2, (const char* Filename),(this, Filename))
     VIRTUAL_METHOD(const char*, GetModelName, 3, (const void* Model),(this, Model))
 };
